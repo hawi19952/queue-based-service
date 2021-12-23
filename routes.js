61709f6a9dbc3c3e1ed0dec7ce9cb3d3de63c796
hawi19952 <hussain@mohtwize.com>
2021-12-23 23:37:04 +0300

    generate the model and the API end point get / post

---
+++ routes.js
@@ -0,0 +1,27 @@
+const express = require('express'); 
+const queueModel = require('./models');
+
+const app = express(); 
+
+app.post("/add_queue_item", async (request, response) => {
+    const queueItem = new queueModel(request.body); 
+
+    try {
+        await queueItem.save(); 
+        response.send(queueItem);
+    } catch (error) {
+        response.status(500).send(error); 
+    }
+}); 
+
+app.get("/queue_items", async (request, response) => {
+    const queueItems = await queueModel.find({}); 
+
+    try {
+        response.send(queueItems);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+})
+
+module.exports = app; 
