078aef3d33fd5950c23bc7bc1f8eb96547fc339a
hawi19952 <hussain@mohtwize.com>
2021-12-23 23:25:24 +0300

    setup using express and mongoose

---
+++ index.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv').config(); 
+const express = require('express');
+const mongoose = require('mongoose');
+const Router = require('./routes');
+
+//TODO: 1) create a universal string for connection based on user input for env
+
+const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/mydb?retryWrites=true&w=majority'; 
+const queueName = process.env.QUEUE_NAME || 'test'; 
+const port = process.env.PORT || '4100'; 
+
+const app = express(); 
+
+app.use(express.json())
+
+mongoose.connect(uri, 
+    {
+        useNameUrlParser: true, 
+        useFindAndModify: false, 
+        useUnifiedTopology: true
+    }
+);
+
+const db = mongoose.connection; 
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", () => {
+    console.log("Connected Successfully! ");
+})
+
+app.use(Router); 
+
+app.listen(port, () => {
+    console.log(`Server is connected to port ${port}`);
+});
+
--- src/generate.queue.js
+++
@@ -1,6 +0,0 @@
-const dotenv = require('dotenv').config(); 
-const mongodb = require('mongodb')
-
-const MongoClient = mongodb.MongoClient;
-const URI =  process.env.MONGO_URI || "mongodb://localhost:27017/mydb"; 
-
